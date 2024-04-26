c0fbbdc5868342f502c2528cf372b1ad61852039
lh2080028463 <2080028463@qq.com>
2024-04-26 13:09:44 +0800

    修改场地排序的bug

--- 基础功能.c
+++ 基础功能.c
@@ -5,18 +5,19 @@
 #include"管理员.h"
 #include<string.h>
 #include<math.h>
+#include<search.h>
 
 extern Reservation Reservations[10000];
 extern Field* FieldRoot;
 
 /*预定时间合理*/
-bool checkTime(const Duration reservedTime, const Duration openTime) 
+bool checkTime(const Duration reservedTime, const Duration openTime)
 {
 	if (reservedTime.start.hour > openTime.start.hour ||
-		(reservedTime.start.hour == openTime.start.hour && reservedTime.start.minute >= openTime.start.minute)) 
+		(reservedTime.start.hour == openTime.start.hour && reservedTime.start.minute >= openTime.start.minute))
 	{
 		if (reservedTime.end.hour < openTime.end.hour ||
-			(reservedTime.end.hour == openTime.end.hour && reservedTime.end.minute <= openTime.end.minute)) 
+			(reservedTime.end.hour == openTime.end.hour && reservedTime.end.minute <= openTime.end.minute))
 		{
 			return true;
 		}
@@ -43,9 +44,9 @@ Field* findFieldname(const Field* FieldRoot, const char name[])
 }
 
 /*账号查询*/
-User* findUsername(const User* userRoot,const char username[])
+User* findUsername(const User* userRoot, const char username[])
 {
-	if (userRoot == NULL) 
+	if (userRoot == NULL)
 		return userRoot;
 	User* temp = userRoot;
 	if (strcmp(username, userRoot->username) < 0)
@@ -63,9 +64,9 @@ User* findUsername(const User* userRoot,const char username[])
 /*检查是否被预定*/
 bool rented(Reservation newReservation)
 {
-	for (int i = 0; i < Reservations[i].idx!=0; i++)
+	for (int i = 0; i < Reservations[i].idx != 0; i++)
 	{
-		if (strcmp(newReservation.fieldName, Reservations[i].fieldName) != 0||Reservations[i].deleted)
+		if (strcmp(newReservation.fieldName, Reservations[i].fieldName) != 0 || Reservations[i].deleted)
 		{
 			continue;
 		}
@@ -76,15 +77,96 @@ bool rented(Reservation newReservation)
 				(newReservation.time.end.hour == Reservations[i].time.start.hour &&
 					newReservation.time.end.minute >= Reservations[i].time.start.minute)))
 		{
-			return true; 
+			return true;
 		}
-		
+
 	}
-	return false; 
+	return false;
+}
+
+
+int compareFields0(const void* a, const void* b)
+{
+	const Field* fieldA = (const Field*)a;
+	const Field* fieldB = (const Field*)b;
+	return fieldB->idx - fieldA->idx;
+}
+/*比较函数根据排序条件进行比较*/
+int compareFields1(const void* a, const void* b)
+{
+	const Field* fieldA = (const Field*)a;
+	const Field* fieldB = (const Field*)b;
+	return strcmp(fieldA->name, fieldB->name);
 }
 
 /*比较函数根据排序条件进行比较*/
-int compareFields(const void* a, const void* b, int conditon) 
+int compareFields2(const void* a, const void* b)
+{
+	const Field* fieldA = (const Field*)a;
+	const Field* fieldB = (const Field*)b;
+
+
+
+	if (fieldA->area < fieldB->area) return -1;
+	if (fieldA->area > fieldB->area) return 1;
+	return 0;
+
+}
+/*比较函数根据排序条件进行比较*/
+int compareFields3(const void* a, const void* b)
+{
+	const Field* fieldA = (const Field*)a;
+	const Field* fieldB = (const Field*)b;
+
+
+	if (fieldA->price[0] < fieldB->price[0]) return -1;
+	if (fieldA->price[0] > fieldB->price[0]) return 1;
+
+}/*比较函数根据排序条件进行比较*/
+int compareFields4(const void* a, const void* b)
+{
+	const Field* fieldA = (const Field*)a;
+	const Field* fieldB = (const Field*)b;
+
+
+	if (fieldA->price[1] < fieldB->price[1]) return -1;
+	if (fieldA->price[1] > fieldB->price[1]) return 1;
+}/*比较函数根据排序条件进行比较*/
+int compareFields5(const void* a, const void* b)
+{
+	const Field* fieldA = (const Field*)a;
+	const Field* fieldB = (const Field*)b;
+
+
+	if (fieldA->price[2] < fieldB->price[2]) return -1;
+	if (fieldA->price[2] > fieldB->price[2]) return 1;
+
+}/*比较函数根据排序条件进行比较*/
+int compareFields6(const void* a, const void* b)
+{
+	const Field* fieldA = (const Field*)a;
+	const Field* fieldB = (const Field*)b;
+
+
+	if (fieldA->openTime.start.hour < fieldB->openTime.start.hour) return -1;
+	if (fieldA->openTime.start.hour > fieldB->openTime.start.hour) return 1;
+	if (fieldA->openTime.start.minute < fieldB->openTime.start.minute) return -1;
+	if (fieldA->openTime.start.minute > fieldB->openTime.start.minute) return 1;
+
+}/*比较函数根据排序条件进行比较*/
+int compareFields7(const void* a, const void* b)
+{
+	const Field* fieldA = (const Field*)a;
+	const Field* fieldB = (const Field*)b;
+
+
+	if (fieldA->openTime.end.hour < fieldB->openTime.end.hour) return -1;
+	if (fieldA->openTime.end.hour > fieldB->openTime.end.hour) return 1;
+	if (fieldA->openTime.end.minute < fieldB->openTime.end.minute) return -1;
+	if (fieldA->openTime.end.minute > fieldB->openTime.end.minute) return 1;
+
+}/*比较函数根据排序条件进行比较*/
+int compareFields(const void* a, const void* b, int conditon)
 {
 	const Field* fieldA = (const Field*)a;
 	const Field* fieldB = (const Field*)b;
@@ -99,11 +181,14 @@ int compareFields(const void* a, const void* b, int conditon)
 		if (fieldA->area > fieldB->area) return 1;
 		return 0;
 	case 3://按早晨价格排序
-		return fieldA->price[0] - fieldB->price[0];
+		if (fieldA->price[0] < fieldB->price[0]) return -1;
+		if (fieldA->price[0] > fieldB->price[0]) return 1;
 	case 4://按下午价格排序
-		return fieldA->price[1] - fieldB->price[1];
+		if (fieldA->price[1] < fieldB->price[1]) return -1;
+		if (fieldA->price[1] > fieldB->price[1]) return 1;
 	case 5://按晚上价格排序
-		return fieldA->price[2] - fieldB->price[2];
+		if (fieldA->price[2] < fieldB->price[2]) return -1;
+		if (fieldA->price[2] > fieldB->price[2]) return 1;
 	case 6://按开放时间排序
 		if (fieldA->openTime.start.hour < fieldB->openTime.start.hour) return -1;
 		if (fieldA->openTime.start.hour > fieldB->openTime.start.hour) return 1;
@@ -120,16 +205,76 @@ int compareFields(const void* a, const void* b, int conditon)
 }
 
 /*场地信息排序函数*/
-void sortFields(Field* fields, int numFields, int conditon) 
+void sortFields(Field* tempField, int num)
 {
-	qsort(fields, numFields, sizeof(Field),(int (*)(const void*, const void*))compareFields, conditon);
+	int conditon;
+	while (true)
+	{
+		printf(" ******************场地信息排序****************\n");
+		printf("                 0.取消排序\n");
+		printf("                 1.名称\n");
+		printf("                 2.面积\n");
+		printf("                 3.早上价格\n");
+		printf("                 4.下午价格\n");
+		printf("                 5.晚上价格\n");
+		printf("                 6.开放时间\n");
+		printf("                 7.关闭时间\n");
+		printf(" ******************************************\n");
+		printf("请选择场地信息排序功能：");
+		scanf("%d", &conditon);
+		if (conditon == 0)
+		{
+			break;
+		}
+		else if (conditon > 0 && conditon < 8)
+		{
+			if (conditon == 1)
+			{
+				qsort(tempField, num, sizeof(Field), compareFields1);
+				putFieldMessage(tempField);
+			}
+			else if (conditon == 2)
+			{
+				qsort(tempField, num, sizeof(Field), compareFields2);
+				putFieldMessage(tempField);
+			}
+			else if (conditon == 3)
+			{
+				qsort(tempField, num, sizeof(Field), compareFields3);
+				putFieldMessage(tempField);
+			}
+			else if (conditon == 4)
+			{
+				qsort(tempField, num, sizeof(Field), compareFields4);
+				putFieldMessage(tempField);
+			}
+			else if (conditon == 5)
+			{
+				qsort(tempField, num, sizeof(Field), compareFields5);
+				putFieldMessage(tempField);
+			}
+			else if (conditon == 6)
+			{
+				qsort(tempField, num, sizeof(Field), compareFields6);
+				putFieldMessage(tempField);
+			}
+			else if (conditon == 7)
+			{
+				qsort(tempField, num, sizeof(Field), compareFields7);
+				putFieldMessage(tempField);
+			}
+
+		}
+		else
+		{
+			system("cls");
+			printf("请输入正确的序号!");
+			Sleep(500);
+			system("cls");
+		}
+	}
 }
 
-/*信息统计*/
-void statistic()
-{
-
-}
 
 /*初始化预定信息数组*/
 void initReservations(Reservation r)
@@ -150,7 +295,7 @@ unsigned int calculateTime(Duration dur)
 unsigned int calculatePrice(Reservation reservation)
 {
 	Duration morning = { {0,0},{12,0} };
-	Duration afternoon= { {12,0},{18,0} };
+	Duration afternoon = { {12,0},{18,0} };
 	Duration night = { {18,0} ,{23,59} };
 	Field* tempField = findFieldname(FieldRoot, reservation.fieldName);
 
--- 基础功能.h
+++ 基础功能.h
@@ -9,6 +9,16 @@ void initReservations(Reservation r);
 bool rented(Reservation newReservation);
 bool checkTime(const Duration reservedTime, const Duration openTime);
 int compareFields(const void* a, const void* b, int conditon);
-void sortFields(Field* fields, int numFields, int conditon);
+
 unsigned int calculatePrice(Reservation reservation);
 unsigned int calculateTime(Duration dur);
+
+int compareFields0(const void* a, const void* b);
+int compareFields1(const void* a, const void* b);
+int compareFields2(const void* a, const void* b);
+int compareFields3(const void* a, const void* b);
+int compareFields4(const void* a, const void* b);
+int compareFields5(const void* a, const void* b);
+int compareFields6(const void* a, const void* b);
+int compareFields7(const void* a, const void* b);
+void sortFields(Field* tempField, int num);
